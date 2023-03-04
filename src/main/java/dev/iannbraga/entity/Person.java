4adff181af8210bef632ad7fde649a0feb769669
iannbraga <bragaprestes@gmail.com>
2023-03-03 23:49:02 -0300

    product entity

--- src/main/java/dev/iannbraga/entity/Person.java
+++ src/main/java/dev/iannbraga/entity/Person.java
@@ -13,7 +13,7 @@ import lombok.Data;
 public class Person {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name", length = 60, nullable = false)
--- src/main/java/dev/iannbraga/entity/Product.java
+++ src/main/java/dev/iannbraga/entity/Product.java
@@ -1,5 +1,6 @@
 package dev.iannbraga.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,9 +16,18 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+    
+    @Column(nullable = false)
     private String description;
+    
+    @Column(nullable = false)
     private String category;
+    
+    @Column(nullable = false)
     private Double price;
+    
+    @Column(nullable = false)
     private int stock;
 }
